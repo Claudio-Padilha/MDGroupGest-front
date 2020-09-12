f5b6d00940181b87a90e0d5972a7aa8b0ae424cc
padilhaClaudio <padilha86@gmail.com>
2020-09-12 06:19:53 -0800

    Create office screen done, need positionig correction though

--- mdgroupgest/src/App.js
+++ mdgroupgest/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Login from './screens/Login/login';
 import CreateContract from './screens/CreateContract/createContract';
+import CreateOffice from './screens/CreateOffice/createOffice';
 import TextWithCalendar from './components/TextWithCalendar/textWithCalendar';
 import SwitchButton from './components/ToggleComponent/toggleButton';
 
 function App() {
   return (
-    <Login />
+    <CreateOffice />
   );
 }
 
---
+++ mdgroupgest/src/screens/CreateOffice/styles.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+const Form = styled.form`
+  margin-top: 10%;
+`;
+const MainDiv = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  .jxbNCh {
+    align-self: center;
+    background-color: #FFF;
+    display: flex;
+    width: 25%;
+    height: 40%;
+    position: absolute;
+    top: auto;
+    bottom: 25%;
+    left: 25%;
+    .efmjdB {
+      > div {
+        margin-bottom: 5%;
+      }
+      > button {
+        margin-top: 20%;
+        position: absolute;
+        top: 100%;
+        left: 72%;
+      }
+      justify-content: space-around;
+    }
+  }
+`;
+
+const CornerLeft = styled.span`
+  flex: 1;
+  position: absolute;
+  top: 83%;
+  left: 15%;
+`;
+
+const LogoContainer = styled.span`
+  flex: 1;
+  position: absolute;
+  top: 5%;
+  left: 10%;
+  right: 10%;
+`;
+
+const CornerRight = styled.span`
+  flex: 1;
+  position: absolute;
+  bottom: 83%;
+  right: 15%;
+`;
+
+export { 
+  Form,
+  MainDiv,
+  CornerLeft,
+  LogoContainer,
+  CornerRight
+};
