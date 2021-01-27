202c00b0fa6fe813537990fb28b4c70678f2b21d
Claudio-Padilha <padilha86@gmail.com>
2021-01-27 09:47:25 -0300

    ...

--- mdgroupgest/src/router.js
+++ mdgroupgest/src/router.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, HashRouter } from "react-router-dom";
 
 const Router = ({ routes }) => {
   return (
-    <BrowserRouter>
-      <Switch>
-        {routes.map((route) => {
-          return (
-            <Route exact path={route.path} component={route.component} />
-          );
-        })}
-      </Switch>
-    </BrowserRouter>
+    <HashRouter>
+        <Switch>
+          {routes.map((route) => {
+            return (
+              <Route exact path={route.path} component={route.component} />
+            );
+          })}
+        </Switch>
+    </HashRouter>
   );
 }
 
