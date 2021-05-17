6123e93f3d598b5929800f73748366ad42f4041b
Claudio-Padilha <padilha86@gmail.com>
2021-05-17 11:18:56 -0300

    ;...

--- mdgroupgest/src/components/Form/complexForm.js
+++ mdgroupgest/src/components/Form/complexForm.js
@@ -54,28 +54,9 @@ const CForm = ({
     return initialValues;
   }
 
+  
+
   const Yup = require('yup')
-
-  // key: "name"
-  // key: "nr"
-  // key: "email"
-  // key: "nif"
-  // key: "zipCode"
-  // key: "contact"
-  // key: "address"
-
-  // const validationSchema = Yup.object().shape({
-  //   name: Yup.string(),
-  //   nr: Yup
-  //     .string()
-  //     // .matches(/^[0-9]+$/, "Must be only digits")
-  //     .min(5, 'Must be exactly 5 digits')
-  //     .max(5, 'Must be exactly 5 digits'),
-  //   officeName: Yup.string().required(),
-  //   officeNIPC: Yup.number().test('len', 'Deve ter exatos 9 caracteres', val => val?.toString()?.length === 9),
-  //   officeZipCode: Yup.number().test('len', 'Deve ter exatos 9 caracteres', val => val?.toString()?.length === 9),
-  //   email: Yup.string().email('Tipo de email inválido').required('* Campo Obrigatório'),
-  // });
 
   return (
     <>
@@ -108,74 +89,7 @@ const CForm = ({
                 />
               </StyledForm>
             )
-          }
-          {/* <Form noValidate onSubmit={handleSubmit}>
-
-            <Form.Row>
-
-              <Form.Group as={Col} md="4" controlId="validationFormik101">
-                <Form.Label>First name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="firstName"
-                  value={values?.firstName}
-                  onChange={handleChange}
-                  isValid={touched?.firstName && !errors?.firstName}
-                />
-              </Form.Group>
-
-            </Form.Row>
-
-            <Form.Row>
-
-              <Form.Group as={Col} md="3" controlId="validationFormik105">
-                <Form.Label>Zip</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Zip"
-                  name="zip"
-                  value={values?.zip}
-                  onChange={handleChange}
-                  isInvalid={!!errors?.zip}
-                />
-
-                <Form.Control.Feedback type="invalid" tooltip>
-                  {errors?.zip}
-                </Form.Control.Feedback>
-              </Form.Group>
-
-            </Form.Row>
-
-            <Form.Group>
-              <Form.File
-                className="position-relative"
-                required
-                name="file"
-                label="File"
-                onChange={handleChange}
-                isInvalid={!!errors?.file}
-                feedback={errors?.file}
-                id="validationFormik107"
-                feedbackTooltip
-              />
-            </Form.Group>
-
-            <Form.Group>
-              <Form.Check
-                required
-                name="terms"
-                label="Agree to terms and conditions"
-                onChange={handleChange}
-                isInvalid={!!errors?.terms}
-                feedback={errors?.terms}
-                id="validationFormik106"
-                feedbackTooltip
-              />
-            </Form.Group>
-            <Button type="submit">Submit form</Button>
-          </Form> */}
-
-          
+          } 
         </Formik>
       </CFormContainer>
     </>
@@ -220,7 +134,35 @@ const renderFields = (field, index, formik, formFields) => {
       return (
         <Form.Group as={Col} className={field?.key} >
           <Select
-            onChange={option => {
+            onChange={async (option) => {
+              if (field.key === 'feedbackCall'){
+                if (option.label != 'OK') {
+                  let obj 
+                  let ausente = true
+                  await formFields[index + 1].options.map(el => {
+                    if (el.label == 'OK') {
+                      ausente = false
+                      obj = el
+                    }
+                  })
+                  if (! ausente) {
+                    formFields[index + 1].options.splice(formFields[index + 1].options.indexOf(obj), 1)
+                    console.log(formFields[index + 1].options)
+                  }
+                }else{
+                  let presente = false
+                  console.log(formFields[index + 1].options)
+                  await formFields[index + 1].options.map(el => {
+                    if (el.label == 'OK') {
+                      presente = true
+                    }
+                  })
+
+                  if (! presente) {
+                    // Pegar vetor de estado da venda da store e colocar no option
+                  }
+                }
+              }
               formik.setFieldValue(field.key, option.value);
             }}
             options={field.options}
